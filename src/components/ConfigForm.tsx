4899c841c164bc2bbb8511956e9c8f445d15bb90
Cas Dekkers <cas.dekkers@ictu.nl>
2020-06-02 06:03:02 +0200

    Update config in accordance with state

--- src/components/ConfigForm.tsx
+++ src/components/ConfigForm.tsx
@@ -3,17 +3,24 @@ import { Card, Col, Form, Row, Collapse } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { configSelector, toggleUnestimatedIssues, setPastSprints, setUnestimatedSize } from '../slices/config';
+import { dataSelector } from '../slices/data';
 
 const ConfigForm = () => {
 
   const dispatch = useDispatch();
-  const { pastSprints, showUnestimatedIssues, unestimatedSize } = useSelector(configSelector);
+  const { board, pastSprints, showUnestimatedIssues, unestimatedSize } = useSelector(configSelector);
+  const { sprints } = useSelector(dataSelector);
 
   const pastSprintsNumber = useRef(null);
   const pastSprintsRange = useRef(null);
 
   const handlePastSprintChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(event.target.value);
+    (pastSprintsNumber.current as unknown as HTMLInputElement).value = event.target.value;
+    (pastSprintsRange.current as unknown as HTMLInputElement).value = event.target.value;
+  }
+
+  const handlePastSprintMouseUp = (event: React.MouseEvent<HTMLInputElement>) => {
+    const value = parseInt((event.target as HTMLInputElement).value);
     if (isNaN(value)) return;
     dispatch(setPastSprints(value));
   }
@@ -30,6 +37,12 @@ const ConfigForm = () => {
     (pastSprintsRange.current as unknown as HTMLInputElement).value = pastSprints.toString();
   }, [pastSprints]);
 
+  useEffect(() => {
+    if (!sprints.length) return;
+    (pastSprintsNumber.current as unknown as HTMLInputElement).max = sprints.length.toString();
+    (pastSprintsRange.current as unknown as HTMLInputElement).max = sprints.length.toString();
+  }, [sprints])
+
   return(
     <Form>
       <Form.Group controlId="pastSprints">
@@ -38,10 +51,10 @@ const ConfigForm = () => {
           <Col className="d-flex align-items-center pr-0" xs="3" sm="2" lg="1">
             <Form.Control
               defaultValue={pastSprints}
-              disabled              
-              max={20} //todo
-              min={1}
+              disabled={!sprints.length}
+              min={sprints.length ? 1 : 0}
               onChange={handlePastSprintChange}
+              onMouseUp={handlePastSprintMouseUp}
               ref={pastSprintsNumber}
               size="sm"
               step={1}
@@ -50,11 +63,11 @@ const ConfigForm = () => {
           <Col className="d-flex align-items-center" xs="9" sm="10" lg="11">
             <Form.Control
               custom
-              disabled
               defaultValue={pastSprints}
-              max={20}
-              min={1}
+              disabled={!sprints.length}
+              min={sprints.length ? 1 : 0}
               onChange={handlePastSprintChange}
+              onMouseUp={handlePastSprintMouseUp}
               ref={pastSprintsRange}
               step={1}
               type="range" />
@@ -65,7 +78,7 @@ const ConfigForm = () => {
         <Form.Label>Unestimated issues</Form.Label>
         <Form.Check
           defaultChecked={showUnestimatedIssues}
-          disabled
+          disabled={!board}
           id="showUnestimatedIssues"
           label="Show unestimated issues"
           onClick={() => dispatch(toggleUnestimatedIssues())}
@@ -78,7 +91,7 @@ const ConfigForm = () => {
               <Form.Label>Number of story points</Form.Label>
               <Form.Control
                 defaultValue={unestimatedSize}
-                disabled
+                disabled={!board}
                 min={0}
                 onChange={handleUnestimatedSizeChange}
                 type="number" />
